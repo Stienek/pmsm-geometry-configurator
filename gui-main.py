d9e41857492352eaf7f3c608125e9eb6d87c151c
Stienek <laurin.weitzel@gmail.com>
2023-10-10 02:23:27 +0200

    Working on GUI

--- gui-main.py
+++ gui-main.py
@@ -1,4 +1,4 @@
-import sys, os
+import sys, os, shutil
 import json
 import zlib
 
@@ -29,22 +29,26 @@ class SettingsManager:
                 self.data = json.load(file)
         except FileNotFoundError:
             self.data = initial_settings
+        self.save()
 
     def __getitem__(self, key):
         return self.data[key]
 
     def __setitem__(self, key, value):
-        self.data[key] = value
-        with open(self.file_path, 'w') as file:
-            json.dump(self.data, file)
+        self.data[key] = value        
+        self.save()
 
     def update(self, new_data):
         self.data.update(new_data)
-        with open(self.file_path, 'w') as file:
-            json.dump(self.data, file)
+        self.save()
 
     def __repr__(self):
         return repr(self.data)
+    
+    def save(self):
+        with open(self.file_path, 'w') as file:
+            json.dump(self.data, file)
+
 
 
 class PMSMFileHandler:
@@ -91,6 +95,20 @@ class PMSMFileHandler:
 class FileWidget(QSplitter):
     def __init__(self, parent, name=None, path=None):
         super().__init__(parent)
+        
+        if path == None:
+            if name == None:
+                raise AttributeError("FileWidget either needs a name or a path to a project.")
+            self.tmp_project_path = os.path.join('tmp', name)
+            os.mkdir(self.tmp_project_path)
+            
+            
+        else:
+            if not os.path.exists(path):
+                raise FileNotFoundError("The specified project does not exist.")
+            self.tmp_project_path = os.path.basename(path).split('.')[0]
+            
+        self.settings = SettingsManager(os.path.join(self.tmp_project_path, "settings.json"), initial_settings={'project_name':name})
 
         self.setSizePolicy(
             QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding
@@ -99,7 +117,7 @@ class FileWidget(QSplitter):
         self.hlayout = QSplitter(Qt.Orientation.Horizontal)
         self.addWidget(self.hlayout)
 
-        self.hlayout.addWidget(self.tree_widget)
+        # self.hlayout.addWidget(self.tree_widget)
 
         self.hlayout.setStretchFactor(0, 1)
         self.hlayout.setStretchFactor(1, 2)
@@ -141,6 +159,11 @@ class MainWindow(QMainWindow):
         self.open_file_action.triggered.connect(self.open_project) 
         self.open_file_action.setShortcut("Ctrl+o") 
 
+        self.save_file_action = QAction('Save Project', self)
+        self.save_file_action.setStatusTip("Save the current project") 
+        self.save_file_action.triggered.connect(self.request_save_project) 
+        self.save_file_action.setShortcut("Ctrl+s") 
+
         self.close_file_action = QAction('Close Project', self)
         self.close_file_action.setStatusTip("Close the project that's shown right now") 
         self.close_file_action.triggered.connect(self.close_project) 
@@ -152,10 +175,18 @@ class MainWindow(QMainWindow):
         self.file_menu.addAction(self.open_file_action)
         self.file_menu.addAction(self.close_file_action)
 
-        self.setStatusBar(QStatusBar(self))
+        self.statusBar = QStatusBar()
+        self.setStatusBar(self.statusBar)
+
+        if os.path.exists('tmp'):
+            shutil.rmtree('tmp')
+        os.makedirs('tmp')
 
         if self.settings['last_project'] is not None:
             self.open_project(self.settings['last_project'])
+        else:
+            self.new_project()
+        self.statusBar.showMessage("Welcome to the PMSM Rotor Geometry Analyzer")
 
 
     def new_project(self):
@@ -193,6 +224,18 @@ class MainWindow(QMainWindow):
         active_widget_index = self.main_widget.indexOf(active_widget)
         self.file_list.pop(active_widget_index)
         self.main_widget.removeTab(active_widget_index)
+        return 0
+    
+    def request_save_project(self, index=None):
+        if index is None:
+            active_widget = self.main_widget.currentWidget()
+        else:
+            active_widget = self.main_widget.widget(index)
+        try:
+            active_widget.save()
+        except AttributeError:
+            return 1
+        self.statusBar.showMessage("Project was saved successfully")
         return 0
     
     def closeEvent(self, a0: QCloseEvent | None) -> None:
